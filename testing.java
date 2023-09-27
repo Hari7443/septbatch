25eb608b5f5b0ecd32fce160feb427452a9867e6
Kasireddy venkata harinadhreddy <108318209+Hari7443@users.noreply.github.com>
2023-09-27 09:40:57 +0530

    Create testing.java
    program using class and object

---
+++ testing.java
@@ -0,0 +1,13 @@
+public class hari
+{
+  public void sum(int a,int b)
+  {
+    int s = a+b;
+    System.out.println(s);
+  }
+  public static void main(String args[]){
+    hari h = new hari();
+    h.sum(10,20);
+  }
+}
+    
